7b4da0cc237d9a2c90446a881bd1c3ef9639dff3
nikhilgangeri <nikhilgangeri@gmail.com>
2025-05-22 23:38:53 +0530

    Completed Step11 to Step20 in PyramidGenerator

--- PyramidGenerator.js
+++ PyramidGenerator.js
@@ -1,7 +1,4 @@
 let character = 'Hello';
-console.log(character);
-character = 'World';
-console.log(character);
-let secondCharacter;
-secondCharacter = "Test";
-console.log(secondCharacter);
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
