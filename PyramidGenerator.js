b495d0d6b57066334e9fc76e6cb6d7d610c2e42a
nikhilgangeri <nikhilgangeri@gmail.com>
2025-05-25 17:18:17 +0530

    Completed Step41 to Step60 in PyramidGenerator

--- PyramidGenerator.js
+++ PyramidGenerator.js
@@ -4,8 +4,21 @@ const rows = [];
 console.log(rows);
 
 for (let i = 0; i < count; i = i + 1){
-    rows.push(i);
+    rows.push(character.repeat(i + 1));
 }
+
+function padRow(name) {
+    const test = "Testing";
+    console.log("This works!");
+    return test;
+    console.log("This works!");
+}
+const call = padRow("Nikhil");
+console.log(call);
 
 let result = "";
 console.log(result);
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
