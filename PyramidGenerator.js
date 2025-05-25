db948ea88701926b41c66ef0382c1db1f7903351
nikhilgangeri <nikhilgangeri@gmail.com>
2025-05-25 19:04:00 +0530

    Completed Step61 to Step70 in PyramidGenerator

--- PyramidGenerator.js
+++ PyramidGenerator.js
@@ -4,18 +4,14 @@ const rows = [];
 console.log(rows);
 
 for (let i = 0; i < count; i = i + 1){
-    rows.push(character.repeat(i + 1));
+    rows.push(padRow(i + 1, count));
 }
 
-function padRow(name) {
-    const test = "Testing";
-    console.log("This works!");
-    return test;
-    console.log("This works!");
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-const call = padRow("Nikhil");
-console.log(call);
-
+padRow();
+console.log(rows);
 let result = "";
 console.log(result);
 
