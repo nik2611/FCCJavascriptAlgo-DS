98193699b7d4c8a146553130339665c727a40e18
nikhilgangeri <nikhilgangeri@gmail.com>
2025-05-26 22:51:13 +0530

    Completed Step71 to Step100 in PyramidGenerator

--- PyramidGenerator.js
+++ PyramidGenerator.js
@@ -1,20 +1,28 @@
 const character = "#";
 const count = 8;
 const rows = [];
-console.log(rows);
-
-for (let i = 0; i < count; i = i + 1){
-    rows.push(padRow(i + 1, count));
-}
 
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-padRow();
-console.log(rows);
+
+// change the code to a different kind of loop
+/* for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count));
+} */
+
+/* while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+} */
+
+for (let i = count; i > 0; false) {
+
+}
+
 let result = "";
-console.log(result);
 
 for (const row of rows) {
   result = result + row + "\n";
 }
+
+console.log(result);
